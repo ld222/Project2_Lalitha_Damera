653fdbe0b091c98bf09f81a8f44872e16aadfe77
Lalitha Damera <dameralalitha@gmail.com>
2021-09-06 18:45:53 +1000

    Fixed modal issue

--- contact_formhandler.js
+++ contact_formhandler.js
@@ -5,11 +5,11 @@ const phone = document.getElementById("phone")
 const email = document.getElementById("email")
 const message = document.getElementById("message")
 const form = document.getElementById("formdata")
-const submitform = document.getElementById("submitmodal")
-const body = document.getElementById("body")
+const body = document.querySelector("body")
 const overlay = document.getElementById("overlay")
 const modal = document.getElementById("modal")
 const closebutton = document.getElementById("closemodal")
+const buttonoverlay = document.getElementsByClassName("buttonoverlay")
 let inputs = document.getElementsByClassName("inputs")
 // let errmsg = ""
 
@@ -27,25 +27,26 @@ form.onsubmit = submitFormData
 function submitFormData(event){
     event.preventDefault() /*Prevents default initial refresh*/
     
-    // const items = [...document.querySelectorAll("input[type=text]")];
     for (let i = 0; i < inputs.length; i++) {
         if((inputs[i].value == "") && (inputs[i].id != "phone")) {
             // console.log('Enter mandatory fields')
             inputs[i].style.borderColor == "red"            
-            }
-           
+            }           
     }
-    
+    let checkField = 0
     //  Validate all the input data in the form       
-    if(validInput(regname, fname) && (fname.value.length > 0)){
-        console.log({FirstName:fname.value})
+    if(validInput(regname, firstname) && (firstname.value.length > 0)){
+        checkField = 1;
+        console.log({FirstName:firstname.value})
+
     }
     else{
         console.log("Enter valid First Name")
     }
 
-    if(validInput(regname, lname) && (lname.value.length > 0)){
-        console.log({LasttName:lname.value})
+    if(validInput(regname, lastname) && (lastname.value.length > 0)){
+        checkField = 1;
+        console.log({LasttName:lastname.value})
     }
     else{
         console.log("Enter valid Last Name")
@@ -57,25 +58,25 @@ function submitFormData(event){
         console.log("Enter valid phone")
     }
     if(validInput(regemail, email) && (email.value.length > 0)){
+        checkField = 1;
         console.log({Email:email.value})
     }
     else{
         console.log("Enter valid Email")
     }
     if(validInput(regmessage, message) && (message.value.length > 0)){
+        checkField = 1;
         console.log({Message:message.value})
     }
     else{
         console.log("Enter valid Message")
     }    
-    // open modal on submission of form data
-    openModal
     
-    // checkField(firstname, regname)   
-    // checkField(lastname, regname)  
-    // checkField(phone, regphone)  
-    // checkField(email, regemail)  
-    // checkField(message, regmessage)               
+    if ((firstname.value.length > 0)&&(lastname.value.length > 0)&&(email.value.length > 0)&&(message.value.length > 0))
+    {
+        // open modal on submission of form data
+        openModal()
+    }          
 }     
    
    
@@ -106,6 +107,7 @@ function openModal(){
     body.classList.add("modal-active")
     overlay.classList.remove("hidden")
     modal.classList.remove("hidden")
+    buttonoverlay.classList.remove("hidden")
 }
 // close modal on clicking the close button/enter
 function closeModal(){
